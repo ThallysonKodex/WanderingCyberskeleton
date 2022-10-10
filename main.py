d614a9cf54dd1675903fb2d6c0ea60b7744cb14e
ThallysonKodex <thallysontrader@gmail.com>
2022-10-10 02:39:46 -0400

    Added graphical effects

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ class AllAssets(pygame.sprite.Group):
 
         self.level = Level(level_1)
 
+        self.undX, self.undY = 32, 27
+        self.cx, self.cy = 110, 110
+
     def drawing(self, screen):
 
         self.offset.x = player.rect.centerx - WINDOW_WIDTH / 2
@@ -23,10 +26,14 @@ class AllAssets(pygame.sprite.Group):
         self.level.draw_map(self.offset).draw(screen)
         player.particling(screen)
 
-        screen.blit(circle_surf(30, (150, 150, 150)), [234 + 25, 235 + 25], special_flags=pygame.BLEND_RGB_ADD)
-        screen.blit(circle_surf(35, (150, 50, 50)), [234 + 25 - 3, 235 + 25 - 3], special_flags=pygame.BLEND_RGB_ADD)
-        screen.blit(circle_surf(45, (50, 50, 50)), [234 + 25 - 9, 235 + 25 - 9], special_flags=pygame.BLEND_RGB_ADD)
-        screen.blit(circle_surf(45 * 2, (14, 10, 10)), [234 + 25 - (18 + 13), 235 + 25 - (18 + 12)], special_flags=pygame.BLEND_RGB_ADD)
+
+
+        screen.blit(pygame.transform.scale(pygame.image.load("graphics/Underlay.png").convert_alpha(), (self.cx, self.cy)), [WINDOW_WIDTH / 2 - self.undX, WINDOW_HEIGHT / 2 - self.undY])
+
+
+
+
+
         for sprite in sorted(self.sprites(), key= lambda a: a.rect.centery):
             offset = sprite.rect.center - self.offset
             screen.blit(sprite.image, offset)
@@ -60,22 +67,19 @@ items.append(gun1)
 
 
 font = pygame.font.SysFont("Arial", 40)
+mag = pygame.transform.scale(pygame.image.load("graphics/Magnifying.png").convert_alpha(), (WINDOW_WIDTH + 300, WINDOW_HEIGHT + 300))
 
 
 f3 = False
 
 # Test
-def circle_surf(radius, color):
-    surf = pygame.Surface((radius, radius))
-    pygame.draw.ellipse(surf, color, surf.get_rect(), radius)
-    surf.set_colorkey((0, 0, 0))
-    return surf
+
 
 
 
 while True:
 
-    screen.fill((50, 50, 50))
+    screen.fill('red')
 
     dt = clock.tick() / 1000
 
@@ -94,6 +98,9 @@ while True:
     allAssets.drawing(screen)
     allAssets.update(screen, dt, items)
 
+
+
+    screen.blit(mag, mag.get_rect(topleft = (-150, -150)))
 
 
 
